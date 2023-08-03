38b75a2b3687eb9579fa28e81f256d1cce584003
Anizx9171 <anizx9171@gmail.com>
2023-08-03 08:51:15 +0700

    up

--- js/bt1.js
+++ js/bt1.js
@@ -1,10 +1,16 @@
 class Rectangle {
-    constructor(weith, height, area, perimeter) {
+    weith
+    height
+    area
+    perimeter
+    constructor(weith, height) {
         this.weith = weith
         this.height = height
-        this.area = this.weith * this.height
-        this.perimeter = (this.weith + this.height) * 2
+        this.area = () => { console.log(this.weith * this.height) }
+        this.perimeter = () => { console.log((this.weith + this.height) * 2) }
     }
 }
 let squed = new Rectangle(10, 20)
 console.log(squed);
+squed.area()
+squed.perimeter()
